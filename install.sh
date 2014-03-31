151354d764771d4110cfb7bfb3f0ae675044eabb
Philip Blyth <philip.blyth@gmail.com>
2014-03-31 17:21:03 -0400

    Added `install.sh`
    Note:
    this script needs to be configured per shell script.
    ruby scripts get added automatically

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+LOCAL_BIN="/usr/local/bin"
+
+u=false
+
+while [ "$1" != "" ]; do
+  case $1 in
+      -u | --uninstall )  u=true
+                          ;;
+      -h | --help )       echo "Usage: -u | --uninstall : to uninstall"
+                          exit
+                          ;;
+  esac
+  shift
+done
+
+# Concatenate Ruby scripts w/ shell scripts
+declare -a shell_scripts=( *.rb switch cpg_deploy_dev )
+
+for source in ${shell_scripts[@]}; do
+  base=`basename ${source} .rb`
+  target="$LOCAL_BIN/$base"
+
+  if [[ $u = false ]]; then # Installing
+
+    if [[ ! -e "$target" && ! -L "$target" ]]; then
+      ln -s "$source" "$target"
+    else
+      echo "WARNING: $target already exits:"
+    fi
+
+    # Verify
+    ls -l "$target"
+
+  else # Uninstalling
+
+    if [[ -e "$target" || -L "$target" ]]; then
+      rm "$target"
+    else
+      echo "WARNING: $target doesn't exit."
+    fi
+
+  fi
+
+done
